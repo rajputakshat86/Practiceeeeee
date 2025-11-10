de37c32fc8f3fa647fbac1546fb7be661a148011
Akshat Rajput :) <akshatrajput86@gmail.com>
2025-11-10 15:08:23 +0530

    Add files via upload

---
+++ factorial.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int factorial(int number)
+{
+    if (number == 0 || number == 1)
+    {
+        return 1;
+    }
+    else
+    {
+        return (number * factorial(number - 1));
+    }
+}
+int main()
+{
+    int num;
+    printf("Enter the number you want factorial of:\n");
+    scanf("%d", &num);
+
+    printf("Factorial of %d is %d", num, factorial(num));
+
+    return 0;
+}
